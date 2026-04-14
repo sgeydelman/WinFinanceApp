31fc5c916584d347456c5d647ad70ac53f5eb5e5
Sam G <sgeydelman@gmail.com>
2026-04-13 18:39:58 -0700

    Version change

--- WinFinanceApp/Properties/AssemblyInfo.cs
+++ WinFinanceApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.1.12")]
-[assembly: AssemblyFileVersion("1.0.1.12")]
+[assembly: AssemblyVersion("1.0.1.13")]
+[assembly: AssemblyFileVersion("1.0.1.13")]
